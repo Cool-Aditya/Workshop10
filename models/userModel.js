38fc40a11269036b3442df33de23019d035a9a4e
Aditya Raj <aditya@leewayhertz.com>
2021-06-02 16:38:15 +0530

    workshop10

--- models/userModel.js
+++ models/userModel.js
@@ -26,7 +26,7 @@ userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 12);
   }
-  console.log(this.password);
+  // console.log(this.password);
   next();
 });
 
